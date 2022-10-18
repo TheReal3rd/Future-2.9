a6dfa66f28d101084224f9c954ca39ce45ed109c
third <ethanamt2001@gmail.com>
2022-10-18 14:35:33 +0100

    a

--- src/main/java/net/futureclient/client/deof/commands/commands/DupeCMD.java
+++ src/main/java/net/futureclient/client/deof/commands/commands/DupeCMD.java
@@ -1,11 +1,14 @@
 package net.futureclient.client.deof.commands.commands;
 
 import net.futureclient.client.deof.commands.manager.CommandBase;
+import net.minecraft.inventory.ClickType;
+import net.minecraft.item.ItemAir;
+import net.minecraft.util.EnumHand;
 
 public class DupeCMD extends CommandBase {//TODO finish this.
 
     public DupeCMD() {
-        super(new String[] {"Dupe", "DupeExploit", "DupeKick", "DropKick", "DupeAll", "InventoryDupe"});
+        super(new String[]{"Dupe", "DupeExploit", "DupeKick", "DropKick", "DupeAll", "InventoryDupe"});
     }
 
     @Override
@@ -15,61 +18,32 @@ public class DupeCMD extends CommandBase {//TODO finish this.
 
     @Override
     public String execute(String[] object) {
-        int n;
-        block15: {
-            if (0.f$d.isSingleplayer()) {
-                return "You are in singleplayer.";
-            }
-            if (((Object)object).length != 1) {
-                return null;
-            }
-            object = ((String)object[0]).toLowerCase();
-            int n2 = -1;
-            switch (((String)object).hashCode()) {
-                case 3194991: {
-                    if (!((String)object).equals("hand")) break;
-                    n = n2 = 0;
-                    break block15;
-                }
-                case 3198835: {
-                    if (!((String)object).equals("hend")) break;
-                    n = n2 = 1;
-                    break block15;
-                }
-                case 96673: {
-                    if (!((String)object).equals("all")) break;
-                    n = n2 = 2;
-                    break block15;
-                }
-                case -2020599460: {
-                    if (!((String)object).equals("inventory")) break;
-                    n2 = 3;
-                }
-            }
-            n = n2;
+        if (mc.isSingleplayer()) {
+            return "You are in singleplayer.";
         }
-        switch (n) {
-            case 0:
-            case 1: {
-                if (!(0.f$d.player.func_184586_b(EnumHand.MAIN_HAND).getItem() instanceof ItemAir)) {
-                    0.f$d.player.dropItem(true);
-                    0.f$d.world.sendQuittingDisconnectingPacket();
+        if (object.length != 1) {
+            return null;
+        }
+
+        switch (object[0].toLowerCase()) {
+            case "hand":
+            case "hend":
+                if (!(mc.player.getHeldItem(EnumHand.MAIN_HAND).getItem() instanceof ItemAir)) {
+                    mc.player.dropItem(true);
+                    mc.world.sendQuittingDisconnectingPacket();
                     return "Dropping and kicking.";
                 }
-            }
-            return "Put an item in your hand.";
-            case 2:
-            case 3: {
-                int n3 = object = 9;
+                return "Put an item in your hand.";
+            case "all":
+            case "inventory":
+                int slot = 9;
                 while (true) {
-                    if (n3 >= 45) {
-                        0.f$d.world.sendQuittingDisconnectingPacket();
+                    if (slot >= 45) {
+                        mc.world.sendQuittingDisconnectingPacket();
                         return "Dropping and kicking.";
                     }
-                    0.f$d.playerController.windowClick(0, object++, 1, ClickType.THROW, (EntityPlayer)0.f$d.player);
-                    n3 = object;
+                    mc.playerController.windowClick(0, slot++, 1, ClickType.THROW, mc.player);
                 }
-            }
         }
         return null;
     }
--- src/main/java/net/futureclient/client/deof/commands/commands/HClipCMD.java
+++ src/main/java/net/futureclient/client/deof/commands/commands/HClipCMD.java
@@ -11,18 +11,14 @@ public class HClipCMD extends CommandBase {
     public String execute(String[] stringArray) {
         if (stringArray.length == 1) {
             double d = Double.parseDouble(stringArray[0]);
-            di di2 = 0;
-            double d2 = Math.cos(Math.toRadians(di2.f$d.player.field_70177_z + 90.0f));
-            double d3 = Math.sin(Math.toRadians(di2.f$d.player.field_70177_z + 90.0f));
-            if (di2.f$d.player.func_184187_bx() != null) {
-                0.f$d.player.func_184187_bx().setPosition(0.f$d.player.func_184187_bx().posX + (1.0 * d * d2 + 0.0 * d * d3), 0.f$d.player.func_184187_bx().posY, 0.f$d.player.func_184187_bx().posZ + (1.0 * d * d3 - 0.0 * d * d2));
+            double d2 = Math.cos(Math.toRadians(mc.player.rotationPitch + 90.0f));
+            double d3 = Math.sin(Math.toRadians(mc.player.rotationYawHead + 90.0f));
+            if (mc.player.getRidingEntity() != null) {
+                mc.player.getRidingEntity().setPosition(mc.player.getRidingEntity().posX + (1.0 * d * d2 + 0.0 * d * d3), mc.player.getRidingEntity().posY, mc.player.getRidingEntity().posZ + (1.0 * d * d3 - 0.0 * d * d2));
             } else {
-                0.f$d.player.func_70107_b(0.f$d.player.field_70165_t + (1.0 * d * d2 + 0.0 * d * d3), 0.f$d.player.field_70163_u, 0.f$d.player.field_70161_v + (1.0 * d * d3 - 0.0 * d * d2));
+                mc.player.setPosition(mc.player.posX + (1.0 * d * d2 + 0.0 * d * d3), mc.player.posY, mc.player.posZ + (1.0 * d * d3 - 0.0 * d * d2));
             }
-            Object[] objectArray = new Object[2];
-            objectArray[0] = d < 0.0 ? "back" : "forward";
-            objectArray[1] = d;
-            return String.format("Teleported %s &e%s&7 block(s).", objectArray);
+        return String.format("Teleported %s &e%s&7 block(s).", d < 0.0 ? "back" : "forward", d);
         }
         return null;
     }
